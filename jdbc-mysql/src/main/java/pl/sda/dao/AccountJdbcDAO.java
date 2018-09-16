5ebfdb7e83749fd427b6c15b238335504bac6e84
Aneta <aneta_d88@o2.pl>
2018-09-16 13:47:08 +0200

    findAll implementation

--- jdbc-mysql/src/main/java/pl/sda/dao/AccountJdbcDAO.java
+++ jdbc-mysql/src/main/java/pl/sda/dao/AccountJdbcDAO.java
@@ -1,14 +1,39 @@
 package pl.sda.dao;
 
+import java.sql.*;
+
 public class AccountJdbcDAO {
 
-    void openConncection() {
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/zad1?autoReconnect=true&useSSL=false&serverTimezone=UTC";
+    public static final String DB_USER = "scott";
+    public static final String DB_PASS = "scott";
+
+
+    Connection openConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
     }
 
-    void closeConncection() {
+    void closeConnection(Connection connection) throws SQLException {
+        connection.close();
     }
 
-    void findAll() {
+
+    void findAll() throws SQLException {
+        final String query = "SELECT * FROM account";
+        Connection connection = openConnection();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(query);
+        while (resultSet.next()) {
+            Long id = resultSet.getLong("account_id");
+            Integer balance = resultSet.getInt("balance");
+            String number = resultSet.getString("number");
+            Date creationDate = resultSet.getDate("creation_date");
+            Date endDate = resultSet.getDate("end_date");
+            System.out.println(id + " " + balance + " " + number + " " + creationDate + " " + endDate);
+        }
+        resultSet.close();
+        statement.close();
+        closeConnection(connection);
     }
 
     void findById() {
--- jdbc-mysql/src/main/java/pl/sda/dao/Main.java
+++ jdbc-mysql/src/main/java/pl/sda/dao/Main.java
@@ -1,6 +1,12 @@
 package pl.sda.dao;
 
+import java.sql.SQLException;
+
 public class Main {
 
+    public static void main(String[] args) throws SQLException {
+        AccountJdbcDAO accountJdbcDAO = new AccountJdbcDAO();
+        accountJdbcDAO.findAll();
+    }
 
 }
