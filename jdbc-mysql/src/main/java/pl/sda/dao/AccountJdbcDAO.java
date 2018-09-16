660cd634e9f0e686c8c7f2e38ffb942da2f15730
Aneta <aneta_d88@o2.pl>
2018-09-16 12:27:06 +0200

    init

---
+++ jdbc-mysql/src/main/java/pl/sda/dao/AccountJdbcDAO.java
@@ -0,0 +1,28 @@
+package pl.sda.dao;
+
+public class AccountJdbcDAO {
+
+    void openConncection() {
+    }
+
+    void closeConncection() {
+    }
+
+    void findAll() {
+    }
+
+    void findById() {
+    }
+
+    void findByNumber() {
+    }
+
+    void findByCreationDate() {
+    }
+
+    void findAllAfterCreationDate() {
+    }
+
+    void findAccountsCount() {
+    }
+}
